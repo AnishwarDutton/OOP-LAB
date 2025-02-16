bded0451774c9ba76880163423d4e07865370b9e
AnishwarDutton <anishwarshar98@gmail.com>
2025-02-17 03:38:58 +0530

    yo

---
+++ src/analyse.java
@@ -0,0 +1,23 @@
+public class analyse {
+    public static void main(String[] args) {
+        int x=10;
+        double y=x;
+        System.out.println(y);
+    }}
+
+  /*  public static void main(String[] args) {
+        double x=10.5;
+        int y=x;
+        System.out.println(y);
+    }
+    public static void main(String[] args) {
+        double x=10;
+        int y=(int) x;
+        System.out.println(y);
+    }
+}
+*/
+
+
+
+
